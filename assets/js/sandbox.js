88e5d0ffa4dcfb3fb99342040337247410ec89c5
Boliboe <iamjohnsomto@gmail.com>
2023-06-29 15:05:26 +0100

    hour,minute and am/pm text added in sandbox.js

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -3,10 +3,10 @@ const hour = document.getElementById('clock-hour'),
       minutes = document.getElementById('clock-minutes'),
       seconds = document.getElementById('clock-seconds')
 
-      const clock = () =>{
-        let date = new Date()
+const clock = () =>{
+    let date = new Date()
 
-        let hh = date.getHours() * 30,
+    let hh = date.getHours() * 30,
             mm = date.getMinutes() * 6,
             ss = date.getSeconds() * 6
 
@@ -18,3 +18,29 @@ const hour = document.getElementById('clock-hour'),
         }
 
 setInterval(clock, 1000)
+
+
+// clock and date text
+const textHour = document.getElementById('text-hour'),
+      textMinutes = document.getElementById('text-minutes'),
+      textAmPm = document.getElementById('text-ampm'),
+      dateDay = document.getElementById('date-day'),
+      dateMonth = document.getElementById('date-month'),
+      dateYear = document.getElementById('date-year')
+
+const clockText = () =>{
+    let date = new Date()
+
+    let hh = date.getHours(),
+        mm = date.getMinutes(),
+        ampm,
+        day = date.getDate(),
+        month = date.getMonth(),
+        year = date.getFullYear
+}
+
+
+
+textHour.innerHTML = `${hh}`
+
+setInterval(clockText, 1000)
