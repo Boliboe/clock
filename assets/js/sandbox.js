f86992591b18eb7994315e57c6938aae847457f0
Boliboe <iamjohnsomto@gmail.com>
2023-06-29 23:30:15 +0100

    media query added and set

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -50,7 +50,7 @@ if(hh < 10){hh = `0${hh}`}
 
 textHour.innerHTML = `${hh}:`
 
-if (mm < 10){mm = `0{mm}`}
+if (mm < 10){mm = `0${mm}`}
 
 textMinutes.innerHTML = mm
 
@@ -65,3 +65,33 @@ dateYear.innerHTML = year
 }
 
 setInterval(clockText, 1000)
+
+// DARK LIGHT THEME CHANGE
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme){
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark theme
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () =>{
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // we save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
